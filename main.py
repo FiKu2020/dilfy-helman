067057bc105228ed95715145023e9d5c4db4a23c
filip k <filip2006kujawa@gmail.com>
2024-01-18 15:37:32 +0100

    proba 1

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def generowanie_klucz_publicznego():
-    pass
+def generowanie_klucz_publicznego(podst_wartosc,klucz_prywatny_osoby,modulo):
+    klucz_publiczny_danej_osoby = (podst_wartosc ** klucz_prywatny_osoby) % modulo
+    return klucz_publiczny_danej_osoby
 def temp():
     pass
+
+modulo = 19
+podst_wartosc = 6

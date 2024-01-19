0baef9818061cc63417cd87f9c59c3eb1c369771
filip k <filip2006kujawa@gmail.com>
2024-01-19 20:10:57 +0100

    AAAAAAAAAAAAAAAaa

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import random
 import base64
-import hashlib
+
 def generowanie_klucz_publicznego(podst_wartosc, klucz_prywatny_osoby, modulo):
     klucz_publiczny_danej_osoby = (podst_wartosc ** klucz_prywatny_osoby) % modulo
     return klucz_publiczny_danej_osoby
-def crackowanie_szyfru():
-    pass
+
 def szyfrowanie_wiad(wiadomosc, klucz_szyfr):
     wiad_w_base64 = base64.b64encode(wiadomosc.encode())
+    klucz_w_base64 = base64.b64encode(klucz_szyfr.encode())
     zaszyfr_wiad = wiad_w_base64.encode(klucz_szyfr).decode()
     return zaszyfr_wiad
-def odzszyfr_wiad(wiadomosc,klucz_szyfr):
-    pass
-modulo = 19
-podst_wartosc = 6
-przyklad = str(input(">"))
-print(szyfrowanie_wiad(przyklad, 10))
+def odzszyfrowanie_wiad(wiadomosc,klucz_szyfr):
+    czytelna_wiad = base64.b64decode(wiadomosc.decode)
+    czytelny_klucz = base64.b64decode(klucz_szyfr.decode)
+
+while True:
+    przyklad = str(input(">"))
+    if przyklad == "stop":
+        break
+    elif przyklad == "klucz":
+        print("podaj: podstawowa wartosc , klucz prywatny oraz wartosc modulo")
+        dalej = input(">")
+        podstawa , klucz , modulo = dalej.split()
+        podstawa = int(podstawa)
+        klucz = int(klucz)
+        modulo = int(modulo)
+        nowy_klucz = generowanie_klucz_publicznego(podstawa,klucz,modulo)
+        print(nowy_klucz)
+    elif przyklad == "szyfruj":
+        pass
+    elif przyklad == "odszyfruj":
+        pass
+    else:
+        print("nie znana komenda")
+        break

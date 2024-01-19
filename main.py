615e0bd560e108f7bdfdaa76ec2d77563f2797c6
filip k <filip2006kujawa@gmail.com>
2024-01-19 14:08:39 +0100

    idk

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import random
+
 def generowanie_klucz_publicznego(podst_wartosc,klucz_prywatny_osoby,modulo):
     klucz_publiczny_danej_osoby = (podst_wartosc ** klucz_prywatny_osoby) % modulo
     return klucz_publiczny_danej_osoby
-def temp():
+def crackowanie_szyfru():
     pass
-
+def szyfrowanie_wiad():
+    pass
 modulo = 19
 podst_wartosc = 6
+
